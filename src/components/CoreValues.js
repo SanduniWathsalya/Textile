f59f43c24ff9f978b9617687af12558aaefd9cb6
SanduniWathsalya <sanduniwathsalya0620@gmail.com>
2024-09-22 22:20:49 +0530

    Add files via upload

---
+++ src/components/CoreValues.js
@@ -0,0 +1,32 @@
+// src/components/CoreValues.js
+import React from 'react';
+import './CoreValues.css';
+
+const CoreValues = () => {
+  return (
+    <section className="core-values">
+       <div className="core-values-container">
+      <h2>Our Core Values</h2>
+      <ul>
+        <li><h3>Quality Craftsmanship:</h3> 
+            <p>We are committed to offering the finest fabrics that ensure durability, comfort, and elegance in every stitch.</p></li>
+        <li><h3>Sustainability:</h3> 
+            <p>We prioritize eco-friendly materials and practices, ensuring that our products and processes minimize environmental impact.</p></li>
+        <li><h3>Customer Satisfaction:</h3> 
+            <p>Our customers are at the heart of everything we do. We strive to provide personalized service, ensuring a delightful shopping experience.</p></li>
+        <li><h3>Innovation and Creativity:</h3> 
+            <p>We embrace the latest trends and technology in textiles, continuously evolving our collections to inspire creativity in our customers.</p></li>
+        <li><h3>Community Support:</h3> 
+            <p> We believe in fostering strong relationships within our community by supporting local artisans and contributing to local initiatives.</p></li>
+        <li><h3>Affordability:</h3> 
+            <p>We offer premium quality fabrics at competitive prices, ensuring that everyone has access to top-quality materials for their projects.</p></li>
+        <li><h3>ntegrity and Trust:</h3>
+            <p> Transparency and honesty define our interactions with customers, suppliers, and partners, building trust through every transaction.</p></li>
+       
+      </ul>
+      </div>
+    </section>
+  );
+};
+
+export default CoreValues;
